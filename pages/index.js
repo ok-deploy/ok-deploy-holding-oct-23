fc99b2a21cc9ca7f7ae8c2b849936152c3bf8a0a
Jack Wild <jack@isjackwild.com>
2023-11-14 17:34:37 +0000

    linked in to use full profile url

--- pages/index.js
+++ pages/index.js
@@ -147,7 +147,7 @@ const Home = ({ SingletonHoldingPage }) => {
                 className={styles.contact}
                 target="_blank"
                 rel="noreferrer"
-                href={`https://www.linkedin.com/company/${linkedIn}`}
+                href={linkedIn}
               >
                 LinkedIn
               </a>
